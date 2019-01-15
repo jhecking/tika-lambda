09b52ca38cb55f96d5e482c7ad1c8a9465f98ae2
Jan Hecking <jhecking@netgaroo.com>
2019-01-15 11:55:06 +0800

    Use separate prefixes for uploaded documents and extracted data

--- src/main/java/extractor/TikaLambdaHandler.java
+++ src/main/java/extractor/TikaLambdaHandler.java
@@ -13,7 +13,6 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.TransformerConfigurationException;
 import org.xml.sax.SAXException;
 
-import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.Parser;
@@ -25,7 +24,7 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -36,6 +35,16 @@ import org.json.simple.JSONObject;
 public class TikaLambdaHandler implements RequestHandler<S3Event, String> {
 
     private LambdaLogger _logger;
+
+    private String sourcePrefix;
+    private String targetPrefix;
+    private String targetExt;
+
+    public TikaLambdaHandler() {
+      this.sourcePrefix = System.getenv("S3_SOURCE_PREFIX");
+      this.targetPrefix = System.getenv("S3_TARGET_PREFIX");
+      this.targetExt = System.getenv("S3_TARGET_EXT");
+    }
 
     public String handleRequest(S3Event s3event, Context context) {
         _logger = context.getLogger();
@@ -50,12 +59,12 @@ public class TikaLambdaHandler implements RequestHandler<S3Event, String> {
             String key = URLDecoder.decode(record.getS3().getObject().getKey().replace('+', ' '), "UTF-8");
 
             // Short-circuit ignore .extract files because they have already been extracted, this prevents an endless loop
-            if (key.toLowerCase().endsWith(".extract.json")) {
+            if (key.endsWith(this.targetExt)) {
               _logger.log("Ignoring extract file " + key);
               return "Ignored";
             }
 
-            AmazonS3 s3Client = new AmazonS3Client();
+            AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
             S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucket, key));
 
             try (InputStream objectData = s3Object.getObjectContent()) {
@@ -67,21 +76,18 @@ public class TikaLambdaHandler implements RequestHandler<S3Event, String> {
                 ObjectMetadata metaData = new ObjectMetadata();
                 metaData.setContentLength(extractLength);
 
-                _logger.log("Saving extract file to S3");
                 InputStream inputStream = new ByteArrayInputStream(extractBytes);
-                String destkey = stripExtension(key);
-                s3Client.putObject(bucket, destkey + ".extract.json", inputStream, metaData);
+
+                String targetKey = key.replaceFirst(this.sourcePrefix, this.targetPrefix);
+                targetKey += this.targetExt;
+                _logger.log("Saving extract file to S3 at s3://" + bucket + "/" + targetKey);
+                s3Client.putObject(bucket, targetKey, inputStream, metaData);
             }
         } catch (IOException | TransformerConfigurationException | SAXException e) {
             _logger.log("Exception: " + e.getLocalizedMessage());
             throw new RuntimeException(e);
         }
         return "Success";
-    }
-
-    public static String stripExtension(final String s)
-    {
-        return s != null && s.lastIndexOf(".") > 0 ? s.substring(0, s.lastIndexOf(".")) : s;
     }
 
     private String doTikaStuff(String bucket, String key, InputStream objectData) throws IOException, TransformerConfigurationException, SAXException {
@@ -98,7 +104,6 @@ public class TikaLambdaHandler implements RequestHandler<S3Event, String> {
       ParseContext parseContext = new ParseContext();
       parseContext.set(Parser.class, parser);
 
-      Tika tika = new Tika();
       Metadata tikaMetadata = new Metadata();
       try {
         // for synthetic transactions
